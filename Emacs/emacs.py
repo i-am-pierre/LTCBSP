39e62a19eccbdf68fdbc548eeadde042b0a9bc51
I am Pierre <i_am_pierre@icloud.com>
2023-08-18 18:42:53 +0800

    Gave up on Emacs, import solution from the book

---
+++ Emacs/emacs.py
@@ -0,0 +1,32 @@
+info = input().split()
+n = int(info[0])
+m = int(info[1])
+
+grid = []
+for i in range(n):
+    # Add '.' on the right to avoid index error when '*' was at right
+    row = list(input()) + ['.']
+    grid.append(row)
+# Add row of '.' on the bottom to avoid index error when '*' was at bottom
+grid.append(['.'] * (m + 1))
+
+rectangles = 0
+
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == '*':
+            rectangles = rectangles + 1
+
+            # Find right edge of rectangle
+            right = j
+            while grid[i][right + 1] == '*':
+                right = right + 1
+
+            # Delete rectangle
+            row = i
+            while grid[row][j] == '*':
+                for k in range(j, right + 1):
+                    grid[row][k] = '.'
+                row = row + 1
+
+print(rectangles)
